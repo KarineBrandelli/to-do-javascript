1b20673d86b801e919db97ae68dd2071c2a2f4a8
Karine Brandelli <karinebrandelli@live.com>
2022-11-23 14:08:10 -0300

    adding addEventListener click to capture input value

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 const input = document.querySelector('.new-content');
 const adicionar = document.querySelector('.fa-plus');
 const lista = document.querySelector('.todo-list');
+
+
+adicionar.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(input.value)
+})
