caa639392177e76a1a528445510c68177fee776b
Karine Brandelli <karinebrandelli@live.com>
2022-11-23 16:10:49 -0300

    saving tasks on localStorage, adding a function that allows the information to be saved when reloading the page

--- js/script.js
+++ js/script.js
@@ -2,27 +2,38 @@ const input = document.querySelector('.new-content');
 const adicionar = document.querySelector('.fa-plus');
 const lista = document.querySelector('.todo-list');
 
+const itens = JSON.parse(localStorage.getItem('itens')) || [];
+
+console.log(itens)
+
+itens.forEach(task => {
+  criaTarefa(task.tarefa);
+});
+
 adicionar.addEventListener('click', (event) => {
   event.preventDefault();
 
-  criaTarefa(input);
+  criaTarefa(input.value);
 
-  console.log('clicou')
+  let novaTarefa = {
+    "tarefa": input.value
+  };
+
+  itens.push(novaTarefa);
+
+  localStorage.setItem('itens', JSON.stringify(itens));
 });
 
 function criaTarefa(input) {
-  const novaTarefa = input.value;
   const template = `
   <div class="list-items">
   <span class="todo-task">
     <i class="fa-regular fa-circle-check"></i>
-    <p class="task-content">${input.value}</p>
+    <p class="task-content">${input}</p>
   </span>
   <i class="fa-solid fa-xmark"></i>
   </div>
   `
 
   lista.innerHTML += template;
-  
-  console.log('adicionou')
 };
