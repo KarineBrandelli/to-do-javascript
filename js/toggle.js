148864db4e28860c85a0e17670c18d70078ae7e5
Karine Brandelli <karinebrandelli@live.com>
2022-11-25 15:47:04 -0300

    feat: implement toggle button

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,7 @@
-const toggleBtn = document.querySelector('.slider'); 
+const toggleBtn = document.querySelector('.slider');
+const darkmodeBg = document.querySelector('#header-bg'); 
 
 toggleBtn.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark-mode");
+  darkmodeBg.classList.toggle("dark-mode-img");
 });
