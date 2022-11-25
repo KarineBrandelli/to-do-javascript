4a0135432ff35f971cd8a4aec3c28a322bbaae3b
Karine Brandelli <karinebrandelli@live.com>
2022-11-25 14:46:55 -0300

    created new file for toggle functions, exported toggle constant of dom.js file

--- js/dom.js
+++ js/dom.js
@@ -3,3 +3,4 @@ export const adicionar = document.querySelector('.fa-plus');
 export const lista = document.querySelector('.todo-list');
 export const contadorTarefas = document.querySelector('.amount');
 export const limpar = document.querySelector('.clear');
+export const toggle = document.querySelector('.toggle-switch'); 
