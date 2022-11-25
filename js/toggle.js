3768d27393921f1bfdb1accdc356a1db1a64ada4
Karine Brandelli <karinebrandelli@live.com>
2022-11-25 14:51:09 -0300

    build: add toggle function

--- js/toggle.js
+++ js/toggle.js
@@ -1,1 +1,7 @@
 import { toggle } from "./dom.js";
+
+const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+function toggleDarkMode(state) {
+  document.documentElement.classList.toggle("dark-mode", state);
+}
